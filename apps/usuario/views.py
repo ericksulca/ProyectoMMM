5a20be2eb3cf447278bfba29d011700ef95eecc7
Erland <33587369+blackadress@users.noreply.github.com>
2018-08-24 20:41:55 -0500

    Modelo mensajes añadido

--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
-from django.contrib import messages
 from django.db import IntegrityError
 from django.db.models import Q
 from django.shortcuts import render, redirect, render_to_response
@@ -22,8 +21,8 @@ def principal_usuario(request):
 def perfil_usuario(request):
     if request.user.is_authenticated:
         oUsuario = Usuario.objects.get(usuario_login_id=request.user.id)
-        oUser=request.user
-        oEntidad=Entidad_bancaria.objects.all()
+        oUser = request.user
+        oEntidad = Entidad_bancaria.objects.all()
     else:
         oUsuario = ''
     return render(request, 'usuario/perfil.html',{'usuario':oUsuario,'user':oUser,'entidad':oEntidad})
@@ -49,7 +48,6 @@ def registrar_usuario(request, dni_referido=''):
             usuario = formUsuario.save(commit=False)
             usuario.usuario_login = user
             usuario.save()
-            messages.info(request, "Gracias por registrarte")
             usuario = authenticate(request, username=username, password=password1)
             login(request, usuario)
 
@@ -78,10 +76,10 @@ def buscar_usuario(request):
     # Función Q importada para hacer consultas complejas, en este caso una consulta con 'OR'
     for busqueda in palabras_busqueda:
         usuarios = Usuario.objects.filter(
-            Q(nombres__contains = busqueda) |
-            Q(apellido_paterno__contains = busqueda) |
-            Q(apellido_materno__contains = busqueda) |
-            Q(dni__contains = busqueda)
+            Q(nombres__startswith = busqueda) |
+            Q(apellido_paterno__startswith = busqueda) |
+            Q(apellido_materno__startswith = busqueda) |
+            Q(dni__startswith = busqueda)
             )
         print(usuarios)
 
@@ -90,6 +88,20 @@ def buscar_usuario(request):
     }
 
     return render(request, 'usuario/buscar.html', context)
+
+
+def validar_email(request):
+    if request.method == 'POST':
+        if 'email' in request.POST:
+            email = request.POST['email']
+        else:
+            email = ''
+    else:
+        email = ''
+    
+    emails = User.objects.values_list('email', flat=True).filter(email=email)
+
+    return render(request, 'usuario/validar_email.html', {'emails': emails})
 
 
 def validar_username(request):
@@ -104,17 +116,3 @@ def validar_username(request):
     users = User.objects.filter(username=username)
     
     return render(request, 'usuario/validar_username.html', {'users': users})
-
-
-def validar_email(request):
-    if request.method == 'POST':
-        if 'email' in request.POST:
-            email = request.POST['email']
-        else:
-            username = ''
-    else:
-        username = ''
-    
-    emails = User.objects.values_list('email', flat=True).filter(email=email)
-
-    return render(request, 'usuario/validar_email.html', {'emails': emails})
