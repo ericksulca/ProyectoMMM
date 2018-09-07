f9b56c52e50a785c83d135d2b053ce84c93be3e2
blackadress <blackadress.01@gmail.com>
2018-09-07 08:29:45 -0500

    Deposito a solicitud implementado:
    No se distingue entre tipo de movimiento delreceptor y emisor
    
    +solucionar aumentando campos de tipo_mov_receptor y tipo_mov_receptor

--- apps/deposito/views.py
+++ apps/deposito/views.py
@@ -52,15 +52,43 @@ def deposito_solicitud(request):
         monto_total = Decimal(request.POST['monto'])
         for solicitud in oSolicitudes:
             if monto_total >= solicitud.monto_faltante:
+                ultima_operacion = Operacion.objects.latest(field_name='fecha')
+                saldo_final_anterior = ultima_operacion.saldo_final
+                monto_operacion = solicitud.monto_faltante
+                Saldo_final_operacion = ultima_operacion.saldo_final + monto_operacion
                 monto_total -= solicitud.monto_faltante
                 solicitud.monto_completado = solicitud.monto
                 solicitud.monto_faltante = 0
                 solicitud.pagado = True
                 solicitud.save()
+                operacion = Operacion(
+                    monto = monto_operacion,
+                    saldo_inicial = saldo_final_anterior,
+                    saldo_final = Saldo_final_operacion,
+                    tipo_movimiento = 'deposito',
+                    usuario_emisor = oUsuario,
+                    usuario_receptor = solicitud.usuario,
+                    solicitud = solicitud
+                )
+                operacion.save()
             else:
+                ultima_operacion = Operacion.objects.latest(field_name='fecha')
+                saldo_final_anterior = ultima_operacion.saldo_final
+                monto_operacion = solicitud.monto_faltante
+                Saldo_final_operacion = ultima_operacion.saldo_final + monto_operacion
                 solicitud.monto_completado += monto_total 
                 solicitud.monto_faltante -= monto_total 
                 solicitud.save()
+                operacion = Operacion(
+                    monto = monto_operacion,
+                    saldo_inicial = saldo_final_anterior,
+                    saldo_final = Saldo_final_operacion,
+                    tipo_movimiento = 'deposito',
+                    usuario_emisor = oUsuario,
+                    usuario_receptor = solicitud.usuario,
+                    solicitud = solicitud
+                )
+                operacion.save()
                 break
     context = {
         'usuario': oUsuario
