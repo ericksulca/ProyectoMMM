17f36dd4d0f47961d674d5b67f8043b751a93677
Isac <smevy4lc3@gmail.com>
2018-10-16 14:27:50 -0500

    detalles de diseño

--- apps/general/models.py
+++ apps/general/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 class General(models.Model):
     inscripcion=models.IntegerField()
+    monto_minimo=models.IntegerField()
     enlace_youtube=models.CharField(max_length=600)
