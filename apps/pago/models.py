5073bc7af4459b2f3cc44aad92f3c3cd51c316d6
Isac <smevy4lc3@gmail.com>
2018-10-18 18:47:31 -0500

    modelo pago

--- apps/pago/models.py
+++ apps/pago/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
+from apps.deposito.models import Operacion
+
 # from apps.usuario.models import Usuario
 # from apps.solicitud.models import Solicitud
 
 # Create your models here.
+
+
+class General(models.Model):
+
+    operacion = models.ForeignKey(Operacion, on_delete=models.PROTECT)
+    # monto_solicitado=models.IntegerField()
+    tasa_interes=models.DecimalField(max_digits=10, decimal_places=2)
+    monto_actual=models.DecimalField(max_digits=10, decimal_places=2)
+    fecha_actual = models.DateTimeField(auto_now=True)
