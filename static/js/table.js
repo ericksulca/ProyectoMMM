8a01d5ea3ef5a8c56bbd07a76ee001dad993833b
Zeitzn <smevy4lc3@gmail.com>
2018-11-29 12:56:05 -0500

    datatable corregido

---
+++ static/js/table.js
@@ -0,0 +1,36 @@
+var español={
+  "sProcessing":"Procesando...",
+  "sLengthMenu":"Mostrar _MENU_ registros",
+  "sZeroRecords":"No se encontraron resultados",
+  "zEmptyTable":"Ningún dato disponible en esta sala",
+  "sInfo":"Mostrando registros del _START_ al _END_ de _TOTAL_ registros",
+  "sInfoEmpty":"Mostrando registros del o al o de un total de 0 registros",
+  "sInfoFiltered":"(filtrando de un total de _MAX_ registros)",
+  "sInfoPostFix":"",
+  "sSearch":"Buscar...",
+  "sUrl":"",
+  "sInfoThousands":"Cargando...",
+  "oPaginate":{
+    "sFirst":"Primero",
+    "sLast":"Último",
+    "sNext":"Siguiente",
+    "sPrevious":"Anterior"
+  },
+  "oAria":{
+    "sSortAscending":": Activar para ordenar la columna de manera ascendente",
+    "sSortDescending":": Activar para ordenar la columna de manera descendente"
+  }
+}
+
+$(document).ready(function(){
+  $('#dataTable').DataTable({
+    "language":español
+  });
+  $('#dataTableModal').DataTable({
+    "language":español
+  });
+  $('.dataTable').DataTable({
+    "language":español
+  });
+}
+)
