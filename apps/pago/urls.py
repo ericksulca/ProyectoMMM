169aeeaa8f4308f3368e5fef1a6a239a500d9b15
Isac <smevy4lc3@gmail.com>
2018-12-10 21:52:03 -0500

    log

--- apps/pago/urls.py
+++ apps/pago/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
+from django.contrib.auth.decorators import login_required
 from . import views
 app_name='pago'
 
 urlpatterns = [
     path('confirmar/', views.confirmar_pago, name='confirmar pago'),
     path('registrar/', views.registrar_pago, name='registrar pago'),
-    path('', views.index, name='lista_pagos'),
+    path('', login_required(views.index), name='lista_pagos'),
 ]
