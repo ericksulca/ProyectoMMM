d80f579d1d21098f3d19eae12e610b9391f888ac
Isac <smevy4lc3@gmail.com>
2019-01-16 17:22:36 -0500

    cambio de admin, debug false en producción

--- sistUno/settings.py
+++ sistUno/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y)oz^1$9&!lb873q_!1pvf-7tu+$lal-3$$yz$44i$20w3qqyi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
